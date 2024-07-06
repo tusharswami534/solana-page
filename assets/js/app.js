2a1b8f2908b0e049d711efd3dcea20629130b476
tusharswami534 <tusharswami17@gmail.com>
2024-07-06 16:09:56 +0530

    page compelet

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,7 @@
 const navbarbtn = () => {
     document.getElementById("nav-name").classList.toggle("show-nvabar")
+    document.getElementById("span-id").classList.toggle("rotate")
+    document.getElementById("d-n").classList.toggle("bg-transparent")
+    document.getElementById("d-n-2").classList.toggle("bg-transparent")
     document.body.classList.toggle("overflow-hidden")
 }
