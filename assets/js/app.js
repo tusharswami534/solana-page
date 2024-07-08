a9d8b4ce07f7732bb2e4ede72afe8e1304677390
tusharswami534 <tusharswami17@gmail.com>
2024-07-08 10:15:02 +0530

    time line add and burger add

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 const navbarbtn = () => {
     document.getElementById("nav-name").classList.toggle("show-nvabar")
-    document.getElementById("span-id").classList.toggle("rotate")
-    document.getElementById("d-n").classList.toggle("bg-transparent")
-    document.getElementById("d-n-2").classList.toggle("bg-transparent")
+    document.getElementById("rotats").classList.toggle("rotate")
+    document.getElementById("d-none").classList.toggle("bg-transparent")
+    document.getElementById("rotats2").classList.toggle("rotate-2")
     document.body.classList.toggle("overflow-hidden")
 }
