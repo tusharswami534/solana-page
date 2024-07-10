368e779c527adce6d3e9a2db967430c8cc9c74b2
tusharswami534 <tusharswami17@gmail.com>
2024-07-10 11:23:58 +0530

    time line edit

---
+++ Solana_files/app.js
@@ -0,0 +1,7 @@
+const navbarbtn = () => {
+    document.getElementById("nav-name").classList.toggle("show-nvabar")
+    document.getElementById("rotats").classList.toggle("rotate")
+    document.getElementById("d-none").classList.toggle("bg-transparent")
+    document.getElementById("rotats2").classList.toggle("rotate-2")
+    document.body.classList.toggle("overflow-hidden")
+}
