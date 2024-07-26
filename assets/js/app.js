c76b70b39fa502d2aa8791b4652ea5f104699552
tusharswami534 <tusharswami17@gmail.com>
2024-07-26 09:09:56 +0530

    HTML and CSS  update done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 const navbarbtn = () => {
     document.getElementById("nav-name").classList.toggle("show-nvabar")
-    document.getElementById("rotats").classList.toggle("rotate")
+    document.getElementById("rotats").classList.toggle("rotate35")
     document.getElementById("transparent").classList.toggle("bg-transparent")
-    document.getElementById("rotats2").classList.toggle("rotate-2")
+    document.getElementById("rotats2").classList.toggle("rotate-35")
     document.body.classList.toggle("overflow-hidden")
 }
 
